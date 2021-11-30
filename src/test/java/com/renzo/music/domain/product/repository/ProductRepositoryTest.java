5c79c922c55fc00bacd7fc09b7ac33dda52ea884
lee soon woo <lswteen@gmail.com>
2021-11-30 16:48:47 +0900

    feature/renzo-001 상품 비지니스 작업 및 테스트 코드 작성

--- src/test/java/com/renzo/music/domain/product/repository/ProductRepositoryTest.java
+++ src/test/java/com/renzo/music/domain/product/repository/ProductRepositoryTest.java
@@ -1,14 +1,44 @@
 package com.renzo.music.domain.product.repository;
 
+import com.renzo.music.domain.product.entity.Product;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 
 @SpringBootTest
 class ProductRepositoryTest {
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Test
     void save_successe(){
-        System.out.println("1111");
+        Product product = Product.createProductBuilder()
+                .name("상품A")
+                .price(9999)
+                .build();
+        Product newProduct = productRepository.save(product);
+        assertEquals("상품A",newProduct.getName());
+        assertEquals(9999,newProduct.getPrice());
     }
+
+//    @Test
+//    void find_product(){
+//        Product product = Product.createProductBuilder()
+//                .name("상품A")
+//                .price(9999)
+//                .build();
+//        Product newProduct = productRepository.save(product);
+//
+//        Product findProduct = productRepository.getById(newProduct.getId());
+//        assertEquals("상품A",findProduct.getName());
+//        assertEquals(9999,findProduct.getPrice());
+//        assertEquals(1,findProduct.getId());
+//
+//    }
 
 }
